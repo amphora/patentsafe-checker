e5b074b15a2a94053b64b348a1e9bb278399474a
Simon Coles <simonc@amphora-research.com>
2010-10-05 20:49:23 +0100

    Initial version of multiple repository checker

---
+++ check_hosted.rb
@@ -0,0 +1,155 @@
+#!/usr/bin/env ruby 
+#
+# == Synopsis 
+#   Script to scan the PatentSafe document repository checking for
+#   document and signature validity. Mainly intended for Amphora's 
+#   Hosted PatentSafe servers (and their backup)
+#
+# == Examples
+#   
+#     ruby check_hosted.rb /base/dir path/suffix
+#
+# == Usage 
+#   check_hosted.rb [options] base_directory path_inside_each_hit
+#
+#   For help use: ruby check_hosted.rb -h
+#
+# == Options
+#   -h, --help          Displays help message
+#   -v, --version       Display the version, then exit
+#   -q, --quiet         Output as little as possible, overrides verbose
+#   -V, --verbose       Verbose output
+#
+# == Author
+#   Amphora Research Systems, Ltd.
+#
+# == Copyright
+#   Copyright (c) 2006-2009 Amphora Research Systems Ltd.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Things we need to process command line arguments
+require 'optparse' 
+require 'rdoc/usage'
+
+# The Repository checker 
+require "pscheck"
+
+
+# This is pretty much copied from the pscheck.rb file
+class App
+  attr_reader :options
+
+  def initialize(arguments, stdin)
+    @arguments = arguments
+    @stdin = stdin
+
+    # set defaults
+    @options = OpenStruct.new
+    @options.verbose = false
+    @options.quiet = false
+  end
+
+  # Parse options, check arguments, then process the command
+  def run
+    if parsed_options? && arguments_valid? 
+      # arguments then the pscheck command itself
+      process_arguments
+      process_command
+    else
+      output_usage
+    end
+      
+  end
+  
+  protected
+  
+    def parsed_options?
+      # specify options
+      opts = OptionParser.new 
+      opts.on('-h', '--help')         { output_help }
+      opts.on('-V', '--verbose')      { @options.verbose = true }  
+      opts.on('-q', '--quiet')        { @options.quiet = true }
+      opts.on('-y', '--year [yyyy]')  { |yyyy| @options.year = yyyy }
+      opts.parse!(@arguments) rescue return false
+      process_options
+      true      
+    end
+
+    # Performs post-parse processing on options
+    def process_options
+      @options.verbose = false if @options.quiet      
+    end
+    
+    def output_options
+      puts "Options:\n"
+      
+      @options.marshal_dump.each do |name, val|        
+        puts "  #{name} = #{val}"
+      end
+    end
+
+    # True if required arguments were provided
+    def arguments_valid?
+      true if @arguments.length == 2 
+    end
+    
+    # Setup the arguments
+    def process_arguments
+      @base_path = ARGV[0] if ARGV[0]
+      @path_suffix = ARGV[1] if ARGV[1]
+    end
+    
+    def output_help
+      puts version_text
+      RDoc::usage() #exits app
+    end
+    
+    def output_usage
+      RDoc::usage('usage') # gets usage from comments above
+    end
+    
+    def output_version
+      puts version_text
+      puts RDoc::usage('copyright')
+    end
+        
+    # This is where the Script actually does something
+    # Get all the directories in base_path and see if there's something in path_suffix
+    # If there is, run against it
+    def process_command      
+      Dir.new(@base_path).each do | z |
+        # This doesn't work on Windows but who cares? We only host on Unix
+        repository_directory = "#{@base_path}/#{z}/#{@path_suffix}"
+        if File.exists?(repository_directory)
+          puts "========================================================================================================================"
+          puts "Running on #{repository_directory}"
+          puts "========================================================================================================================"
+          repo = Repository.new(:base_path => repository_directory, 
+                                :year => nil, 
+                                :known_exceptions => nil,
+                                :verbose => @options.verbose)
+          repo.check
+        else
+          puts "**** No directory #{repository_directory}"
+        end
+        
+      end
+    end    
+end
+
+
+
+# Create and run the application
+app = App.new(ARGV, STDIN)
+app.run
