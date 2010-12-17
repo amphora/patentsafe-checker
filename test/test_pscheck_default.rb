babbb9cda125083f77b1f7ebb4404019d31b50ca
El Skwid (Don Morrison) <elskwid@gmail.com>
2010-12-16 21:37:22 -0800

    Update tests to work with changes to checker when hosted stuff was added

---
+++ test/test_pscheck_default.rb
@@ -0,0 +1,65 @@
+require  File.dirname(__FILE__)+'/test_helper'
+
+class TestPSCheckDefault < Test::Unit::TestCase
+  # code to get us startup before all tests
+  class << self
+    
+    # runs before all tests
+    def startup
+      @@output = `ruby pscheck.rb #{@@dir}`
+    end
+    
+    # runs after all tests
+    def shutdown; end
+    
+    def suite
+      mysuite = super
+      def mysuite.run(*args)
+        TestPSCheckDefault.startup()
+        super
+        TestPSCheckDefault.shutdown()
+      end
+      mysuite
+    end
+  end
+
+  context "pscheck with default verbosity option" do
+    setup do
+      # before each test
+    end
+
+    should "not have the start time" do
+      assert_no_match /PatentSafe Check Start/i, @@output
+    end
+    
+    should "not have validating text" do
+      assert_no_match /validating TEST0100000003S001 at/i, @@output
+    end
+    
+    should "have checked eighty one signatures" do
+      assert_match /Signatures packets checked:\s+81/i, @@output
+    end
+    
+    should "have the errors summary" do
+      assert_match /-- Errors --/i, @@output
+      assert_match /Missing public key:\s+1/i, @@output
+      assert_match /Invalid signature texts:\s+1/i, @@output
+      assert_match /Invalid content hash:\s+1/i, @@output
+      assert_match /Invalid signatures:\s+1/i, @@output
+      # How to test missing OpenSSL?
+      # assert_match /Skipped signatures\*:\s+13/i, @@output
+    end
+    
+    should "have the successful summary" do
+      assert_match /-- Successful checks --/i, @@output
+      assert_match /Public keys found:\s+80/i, @@output
+      assert_match /Signature texts:\s+80/i, @@output
+      assert_match /Content hashes:\s+80/i, @@output
+      # Test missing OpenSSL?
+      # assert_match /Validated signatures\*:\s+0/i, @@output
+    end
+    
+  end
+  
+  
+end
--- test/test_pscheck_quiet.rb
+++ test/test_pscheck_quiet.rb
@@ -36,25 +36,18 @@ class TestPSCheckQuiet < Test::Unit::TestCase
       assert_no_match /validating TEST0100000003S001 at/i, @@output
     end
     
-    should "have checked eighty one signatures" do
-      assert_match /Signatures packets checked:\s+81/i, @@output
+    should "not have the signatures checked count" do
+      assert_no_match /Signatures packets checked:\s+81/i, @@output
     end
     
-    should "have the errors summary" do
-      assert_match /-- Errors --/i, @@output
-      assert_match /Missing public key:\s+1/i, @@output
-      assert_match /Invalid signature texts:\s+1/i, @@output
-      assert_match /Invalid content hash:\s+1/i, @@output
-      assert_match /Invalid signatures:\s+1/i, @@output
+    should "not have the errors summary" do
+      assert_no_match /-- Errors --/i, @@output
       # How to test missing OpenSSL?
       # assert_match /Skipped signatures\*:\s+13/i, @@output
     end
     
-    should "have the successful summary" do
-      assert_match /-- Successful checks --/i, @@output
-      assert_match /Public keys found:\s+80/i, @@output
-      assert_match /Signature texts:\s+80/i, @@output
-      assert_match /Content hashes:\s+80/i, @@output
+    should "not have the successful summary" do
+      assert_no_match /-- Successful checks --/i, @@output
       # Test missing OpenSSL?
       # assert_match /Validated signatures\*:\s+0/i, @@output
     end
