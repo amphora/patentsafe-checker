f20dcc151d7ae61e0d44af389f0797d8e7717fab
Simon Coles <simonc@amphora-research.com>
2012-01-14 12:18:58 +0000

    Don't require gems

--- check_hosted.rb
+++ check_hosted.rb
@@ -38,9 +38,6 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Sometimes we need Rubygems
-require 'rubygems'
-
 # Things we need to process command line arguments
 require 'optparse' 
 require 'rdoc/usage'
